b9f0744867e428f6e26d0f10b5b248c4616487ca
ntr1556 <ntr1556@rit.edu>
2024-03-21 23:27:03 -0400

    Create functions for strainGauge

--- src/SGM.cpp
+++ src/SGM.cpp
@@ -1,5 +1,18 @@
 #include <SGM.hpp>
 
+namespace IO = EVT::core::IO;
+
 namespace SGM {
+    SGM::SGM(EVT::core::IO::ADC& adc, uint32_t (*conversion)(uint32_t)) :   adc(adc),
+                                                                            conversion(conversion) {
+    }
+
+    uint32_t SGM::getProcessedData() {
+        return conversion(adc.readRaw());
+    }
+
+    uint32_t SGM::getRawADC() {
+        return adc.readRaw();
+    }
 
 }

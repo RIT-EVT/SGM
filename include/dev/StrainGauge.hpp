954e3b846c16a104515fe80d5ff6ef42b0660351
ntr1556 <144548290+ntr1556@users.noreply.github.com>
2024-04-15 20:53:14 -0400

    Update and rename strainGauge.hpp to StrainGauge.hpp

--- include/dev/StrainGauge.hpp
+++ include/dev/StrainGauge.hpp
@@ -1,5 +1,5 @@
-#ifndef strainGauge_STRAINGAUGE_HPP
-#define strainGauge_STRAINGAUGE_HPP
+#ifndef _STRAINGAUGE_HPP
+#define _STRAINGAUGE_HPP
 
 #include <EVT/io/ADC.hpp>
 
@@ -7,7 +7,7 @@ namespace IO = EVT::core::IO;
 
 namespace SGM::DEV {
 
-class strainGauge {
+class StrainGauge {
 public:
     /**
      * Create a new strain gauge which will use the provided ADC and
@@ -17,7 +17,7 @@ public:
      * @param[in] adc The ADC to use to convert ADC values into strain/stress
      * @param[in] conversion The conversion function
      */
-    explicit strainGauge(EVT::core::IO::ADC& adc, uint32_t (*conversion)(uint32_t));
+    explicit StrainGauge(EVT::core::IO::ADC& adc, uint32_t (*conversion)(uint32_t));
 
     /**
      * Return the processed data of the strain gauge
@@ -34,12 +34,12 @@ public:
     uint32_t getRawADC();
 
 private:
-    /// The ADC interface to read from
+    /** The ADC interface to read from */
     EVT::core::IO::ADC& adc;
-    /// Function that processes raw ADC values into useful data
+    /** Function that processes raw ADC values into useful data */
     uint32_t (*conversion)(uint32_t);
 };
 
-}// namespace SGM::DEV
+}    // namespace SGM::DEV
 
-#endif//strainGauge_STRAINGAUGE_HPP
+#endif    //_STRAINGAUGE_HPP

e507a8507319be98553b754cfd61a9a4d4585165
ntr1556 <144548290+ntr1556@users.noreply.github.com>
2024-04-15 20:54:28 -0400

    Update and rename strainGauge.cpp to StrainGauge.cpp
    Replaced strainGauge with StrainGauge

---
+++ src/dev/StrainGauge.cpp
@@ -0,0 +1,15 @@
+#include <dev/StrainGauge.hpp>
+
+namespace IO = EVT::core::IO;
+
+namespace SGM::DEV {
+StrainGauge::StrainGauge(EVT::core::IO::ADC& adc, uint32_t (*conversion)(uint32_t)) : adc(adc), conversion(conversion) {}
+
+uint32_t StrainGauge::getProcessedData() {
+    return conversion(adc.readRaw());
+}
+
+uint32_t StrainGauge::getRawADC() {
+    return adc.readRaw();
+}
+};// namespace SGM::DEV
